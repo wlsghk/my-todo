088171bee86da1986b17c12e6b29a180009ca100
baevolution <baevolutiond@gmail.com>
2023-05-31 19:08:37 +0900

    투두 디자인 시작

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import './App.scss';
 import './App.css';
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs'
+import 'dayjs/locale/ko'
 
 function App() {
+
+  const [timer, setTimer] = useState(dayjs());
+  timer.format();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(dayjs())
+    }, 1000);
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [timer]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 컨테이너 */}
+      <div className="wrapper">
+
+        {/* 헤더 */}
+        <header className="header">
+          <h2>toDoList</h2>
+          <p>{timer.format("YYYY. MM. DD HH:mm:ss")}</p>
+        </header>
+
+        {/* 입력창 */}
+        <div className="input">
+          <input className="input-box" />
+          <button>입력</button>
+        </div>
+
+      </div>
     </div>
   );
 }
