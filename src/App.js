546f1692fe380f08841070d7f32b2c39029351f2
baevolution <baevolutiond@gmail.com>
2023-06-01 12:25:09 +0900

    리스트 생성, 삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import dayjs from 'dayjs'
 import 'dayjs/locale/ko'
+import { AiFillEdit } from 'react-icons/ai';
 
 function App() {
 
-  const [timer, setTimer] = useState(dayjs());
+  let [timer, setTimer] = useState(dayjs());
   timer.format();
+  let [list, setList] = useState('');
+  let [addList, setAddList] = useState([]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -30,11 +33,38 @@ function App() {
           <p>{timer.format("YYYY. MM. DD HH:mm:ss")}</p>
         </header>
 
+        { }
+
         {/* 입력창 */}
         <div className="input">
-          <input className="input-box" />
-          <button>입력</button>
+          <input onChange={(e) => {
+            setList(e.target.value)
+          }} className="input-box" />
+          <button onClick={() => {
+            let copyList = [...addList];
+            copyList.unshift(list);
+            setAddList(copyList);
+            console.log(addList);
+          }}><AiFillEdit size="16" color="534444" /></button>
         </div>
+
+        {/* 할 일 목록 */}
+        {
+          addList.map(function (item, idx) {
+            return (
+              <div className="todo" key={idx}>
+                <p className="todo-box">
+                  <p className="todo-list">{addList[idx]}</p>
+                  <div>{timer.format("YYYY. MM. DD")} <button>수정</button><button onClick={() => {
+                    let delList = [...addList];
+                    delList.splice(idx, 1);
+                    setAddList(delList);
+                  }}>삭제</button></div>
+                </p>
+              </div>
+            )
+          })
+        }
 
       </div>
     </div>
